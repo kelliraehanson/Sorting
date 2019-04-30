f3ec06c5e774f61a0e93854f578b3484d0c8d3ec
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-04-30 17:48:47 -0500

    day 2 mvp

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,8 +1,28 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    merged_arr = []
+    # merged_arr = [0] * elements
+    # a = 0
+    # b = 0
     # TO-DO
+    while len(arrA) > 0 and len(arrB) > 0:
+      smallest = min(arrA[0], arrB[0])
+      if smallest == arrA[0]:
+        arrA.pop(0)
+        merged_arr.append(smallest)
+      else:
+        arrB.pop(0)
+        merged_arr.append(smallest)
+    if len(arrA) > 0:
+      for i in arrA:
+        merged_arr.append(i)
+    else:
+      for i in arrB:
+        merged_arr.append(i)
+    
+    # print(merged_arr)
+    print(f'The Recursive Sort is: {merged_arr}')
     
     return merged_arr
 
@@ -10,8 +30,14 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) > 1:
+        left = merge_sort(arr[0:int(len(arr)/2)])
+        right = merge_sort(arr[int(len(arr)/2):])
+        arr = merge(left, right)
 
     return arr
+
+merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
 
 
 # STRETCH: implement an in-place merge sort algorithm
